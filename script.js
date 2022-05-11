5d74e7068a6526831406b6b78da2bca7af1ca0b6
OreskaG <Ctepanrfo@gmail.com>
2022-05-12 01:23:53 +0300

    fix: active Z index

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-/* <img src="./src/butterflies.jpg" alt="" class="butterflies screenshot">
-<img src="./src/left.jpg" alt="" class="left screenshot">
-<img src="./src/middle.jpg" alt="" class="middle screenshot">
-<img src="./src/throne.png" alt="" class="throne screenshot">
-<img src="./src/tunnel.png" alt="" class="tunnel screenshot"> */
+// const Middle = document.getElementById('middle');
+// const Butterflies = document.getElementById('butterflies');
+// const Left = document.getElementById('left');
+// const Throne = document.getElementById('throne');
+// const Tunnel = document.getElementById('tunnel');
+const ScreenShot = [...document.querySelectorAll('.screenshot')]
 
-const Middle = document.getElementById('middle');
-const Butterflies = document.getElementById('butterflies');
-const Left = document.getElementById('left');
-const Throne = document.getElementById('throne');
-const Tunnel = document.getElementById('tunnel');
-
-function activeImg1 () {
-    Middle.classList.toggle('active');
+function activeImg () {
+    this.classList.toggle('active');
+    setTimeout(() => {
+        this.classList.toggle('activeZIndex');
+    }, '500');
 }
-Middle.addEventListener('click', activeImg1)
 
-function activeImg2 () {
-    Left.classList.toggle('active');
+for ( let i = 0 ; i < ScreenShot.length ; i++ ) {
+    ScreenShot[i].addEventListener('click', activeImg)
 }
-Left.addEventListener('click', activeImg2)
-
-function activeImg3 () {
-    Butterflies.classList.toggle('active');
-}
-Butterflies.addEventListener('click', activeImg3)
-
-function activeImg4 () {
-    Throne.classList.toggle('active');
-}
-Throne.addEventListener('click', activeImg4)
-
-function activeImg5 () {
-    Tunnel.classList.toggle('active');
-}
-Tunnel.addEventListener('click', activeImg5)
